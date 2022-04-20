47d51722f9f3f7a3c3a6bb15bde348fd2adcd3da
samuel14luis <luismendoza.samuel@gmail.com>
2022-04-20 14:09:57 -0500

    scaffolding

---
+++ src/test/java/com/bootcamp/personal/active/loan/LoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.personal.active.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
